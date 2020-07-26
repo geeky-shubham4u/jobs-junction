afb18564d30087c71ffb612113c7e5986e1057a2
TechieShubham <shubhamsri99@gmail.com>
2020-07-27 01:48:37 +0530

    Updates

---
+++ precache-manifest.272626172b27e17072852b2b688f4193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395f60388c1e3ca0524f805679025397",
+    "url": "/jobs-junction/index.html"
+  },
+  {
+    "revision": "9e44e3fe6dc4c6730e93",
+    "url": "/jobs-junction/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3ac3da0dc6d4c20d2413",
+    "url": "/jobs-junction/static/js/2.e7e3d3cb.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/jobs-junction/static/js/2.e7e3d3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e44e3fe6dc4c6730e93",
+    "url": "/jobs-junction/static/js/main.1114bf05.chunk.js"
+  },
+  {
+    "revision": "c71f7e2bf70e1d745cce",
+    "url": "/jobs-junction/static/js/runtime-main.56c026ca.js"
+  }
+]);
